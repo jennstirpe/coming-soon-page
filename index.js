dfe474e99f66d9105744afba9fbb7daccdc0e9db
jstirpe <jennstirpe@yahoo.com>
2022-04-06 16:58:12 -0400

    Added format check and error message to email input

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// IF ERROR, CHANGE STYLES & DISPLAY ERROR ICON
+
+const emailInput = document.querySelector("#email");
+const submitBtn = document.querySelector("#submit");
+const errorIcon = document.querySelector("#error-icon");
+const errorMessage = document.querySelector("#error-message");
+
+
+submitBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    if(emailInput.value === "" || !emailInput.value.match(validRegex)) {
+        errorIcon.classList.remove("error");
+        errorMessage.classList.remove("error");
+    } else {
+        errorIcon.classList.add("error");
+        errorMessage.classList.add("error");
+    }
+})
